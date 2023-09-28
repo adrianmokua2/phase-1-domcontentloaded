243a1daa8ed1b05ec9b18bb1a722909d9cfce724
Adrian <adrian.mokua@icloud.com>
2023-09-28 18:54:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+function changeTextOnLoad() {
+    document.addEventListener('DOMContentLoaded', function () {
+      const paragraph = document.querySelector('p');
+      paragraph.textContent = 'This is really cool!';
+    });
+  }
